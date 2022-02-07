c6e9309473cc94bd0dd5488ed889be8852d64ed3
Jahaziel Hernandez Hoyos <jahazielhernandezhoyoz@gmail.com>
2022-02-07 16:02:19 -0500

    main

---
+++ frontend/src/components/Blog.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Blog = () => {
+    return ( <h1>Desde Blog</h1> );
+}
+ 
+export default Blog;
---
+++ frontend/src/components/Inicio.js
@@ -0,0 +1,6 @@
+import React from 'react';
+const Blog = () => {
+    return ( <h1>Desde Inicio</h1> );
+}
+ 
+export default Blog;
