53954a93d458658bafa9c715473166ba56e8c289
Jahaziel Hernandez Hoyos <jahazielhernandezhoyoz@gmail.com>
2022-02-08 13:13:45 -0500

    main

---
+++ frontend/src/components/Gsap.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+const Gsap = () => {
+  return (
+    <>   
+        <div class="Container">
+          <div class="Wrapper">
+            <div class="Info">
+                <div class="CenterInfo">
+                    <h2>Bienvenido a </h2>
+                    <h2>Tecnoacademia del oriente</h2>
+                    <h1></h1>
+                </div>
+            </div>
+            <div class="Imagenes"></div>
+          </div>
+        </div>
+    </>); 
+};
+
+export default Gsap;
