18426a18494bce50b4e4167c84b8bfce1a76657f
Jahaziel Hernandez Hoyos <jahazielhernandezhoyoz@gmail.com>
2022-02-11 13:26:18 -0500

    main

--- frontend/src/components/Gsap.js
+++ frontend/src/components/Gsap.js
@@ -17,10 +17,9 @@ const Gsap = () => {
       {
         duration: 1,
         autoAlpha: 1,
-        delay: 1,
         ease: "none",
         scrollTrigger: {
-          trigger: ".Container",
+          trigger: ".Container1",
           start: "top center+=100",
           toggleActions: "play none none reverse",
           markers: true,
@@ -32,7 +31,7 @@ const Gsap = () => {
 
   return (
     <>
-      <div className="Container">
+      <div className="Container1">
         <div class="row p-4 pb-0 pe-lg-0 pt-lg-5 align-items-center rounded-3 border shadow-lg">
           <div class="col-lg-7 p-3 p-lg-5 pt-lg-3">
             <h1 class="display-4 fw-bold lh-1">¿Que es?</h1>
