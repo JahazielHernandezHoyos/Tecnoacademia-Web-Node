576991f76dd646eb06b684f08f6fba23e1b1fcb5
Jahaziel Hernandez Hoyos <jahazielhernandezhoyoz@gmail.com>
2022-03-12 12:23:08 -0500

    simulacion captura de huella

--- bakendRaspberry/registrarEstudiante.py
+++ bakendRaspberry/registrarEstudiante.py
@@ -3,12 +3,20 @@ from pymongo import MongoClient
 import tkinter as tk
 from os import system
 
+#simulacion entrada de huella
+gx_ref_buffer = ""
+
+#conectar a la base de datos
+client = MongoClient("mongodb+srv://tecnoacademiaADMIN:bBTWBBnFDG2aReok@tecnoacademia.mjfzz.mongodb.net/estudiantes?retryWrites=true&w=majority") 
+baseDeDatos = client["Tecnoacademia"] #Selecciona la base de datos
+collection = baseDeDatos["estudiantes"] #Selecciona la coleccion
+
 #crear ventana tkinter con dos label y un boton de enviar
+ventana = tk.Tk()
+ventana.title("Registrar Estudiante")
+ventana.geometry("640x480")
+
 def registrarEstudiante():
-    #crear ventana
-    ventana = tk.Tk()
-    ventana.title("Registrar Estudiante")
-    ventana.geometry("640x480")
     #crear label
     tiLabel = tk.Label(ventana, text="T.I: ")
     tiEntry = tk.Entry(ventana)
@@ -16,36 +24,41 @@ def registrarEstudiante():
     tiEntry.grid(row=0, column=1)
     
     #crear boton
-    boton = tk.Button(ventana, text="Enviar", command=lambda: registrar(tiEntry.get()))
+    boton = tk.Button(ventana, text="Enviar", command=lambda: Validar(tiEntry.get()))
     #posicionar boton
     boton.grid(row=5, column=1)
     #ejecutar ventana
+
+    #vaciar consola
     system("cls")
     ventana.mainloop()
-    
 
-def registrar(ti):
-    client = MongoClient("mongodb+srv://tecnoacademiaADMIN:bBTWBBnFDG2aReok@tecnoacademia.mjfzz.mongodb.net/estudiantes?retryWrites=true&w=majority") #conecta con la base de datos en la nube
-    baseDeDatos = client["Tecnoacademia"] #Selecciona la base de datos
-    collection = baseDeDatos["estudiantes"] #Selecciona la coleccion
+
+def Validar(ti):
+
     for documento in collection.find({
         "ti": ti
     }):
         system("cls")
-        print(documento)
-        print("Estudiante ya registrado")
+        if documento["huella"] == "":
+            actualizarRegistro(documento["_id"], documento)
+        else:
+            print("Bienvenido " + documento["nombre"] + " Ingresa tu huella con el dedo indice para registrar tu asistencia")
         break
     else:
         system("cls")
-        print("el estudiante no esta en la base de datos")
+        print("El estudiante no esta en la base de datos")
 
-# collection.insert_one({ 
-#     "nombre": "Juan Perez", 
-#     "ti": 1126912183,
-#     "huella": 13,
-#     }) 
-
-
+    
+def actualizarRegistro(id, documento):
+    tk.messagebox.showinfo("Tu huella no esta registrada", "Bienvenido " + documento["nombre"] + " ingresa tu huella con el dedo indice para registrarte en la Base de datos") 
+    huella = gx_ref_buffer #capturar la huella simulacion #################################
+    collection.update_one(
+        {"_id": id},
+        {"$set": {
+            "huella": huella
+        }}
+    )
 
 def main():
     registrarEstudiante()
