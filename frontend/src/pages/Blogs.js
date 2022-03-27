96f832dac5c935ffcaf371234dba37e1b5a2509b
Jahaziel Hernandez Hoyos <jahazielhernandezhoyoz@gmail.com>
2022-03-26 23:13:12 -0500

    Actualizacion

--- frontend/src/pages/Blogs.js
+++ frontend/src/pages/Blogs.js
@@ -4,13 +4,13 @@ const Blogs = () => {
   return (
     <>
       <Nav />
-      <div className="titulosGsap">
+      {/* <div className="titulosGsap">
         <div className="banner" style={{ height: "calc(40vh - 50px)" }}>
           <div className="banner-content">
             <h1 className="text-light">Blogs</h1>
           </div>
         </div>
-      </div>
+      </div> */}
       <iframe style={{width:"100%", height: "1000px"}} src="https://tecnoacademiaoriente.netlify.app/blog" title="description"></iframe>
     </>
   );
