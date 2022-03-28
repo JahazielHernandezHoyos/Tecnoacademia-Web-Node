d5b0ed20739ccc2989847c6049088c64c0b516f4
Jahaziel Hernandez Hoyos <jahazielhernandezhoyoz@gmail.com>
2022-03-27 20:41:37 -0500

    Actualizacion login

--- frontend/src/pages/Login.js
+++ frontend/src/pages/Login.js
@@ -1,9 +1,13 @@
 import Nav from "../components/Nav";
+import logo from "../assets/Logo.png";
 const Login = () => {
     return ( 
         <>
             <Nav />
-            <form className="mt-5 py-5 col-md-10 mx-auto col-lg-5">
+            <form className="mt-1 py-5 col-md-10 mx-auto col-lg-5">
+            <div class="text-center">
+                <img src={logo} height="250"/>
+            </div>
                 <div className="form-group ">
                     <label htmlFor="exampleInputEmail1">Correo electrónico</label>
                     <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="correo@correo.com" />
@@ -12,11 +16,9 @@ const Login = () => {
                     <label htmlFor="exampleInputPassword1">Contraseña</label>
                     <input type="password" className="form-control" id="exampleInputPassword1" placeholder="Contraseña" />
                 </div>
-                <div className="form-group form-check">
-                    <input type="checkbox" className="form-check-input" id="exampleCheck1" />
-                    <label className="form-check-label" htmlFor="exampleCheck1">Recordar</label>
-                </div>
+                <div class="text-center mt-3">
                 <button type="submit" className="btn btn-primary col-4">Iniciar Sesion</button>
+                </div>
             </form>
         </>
      );
